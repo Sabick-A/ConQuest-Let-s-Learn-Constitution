d65e04d9af30d365c00de2834bdf8de96a9b2ab0
mohamed sanib <121166817+mohamedsanib@users.noreply.github.com>
2024-09-17 15:09:30 +0530

    Add files via upload

--- card_game/script.js
+++ card_game/script.js
@@ -85,6 +85,7 @@ function displayResult(data) {
 function animateLife(life) {
     const hearts = document.querySelectorAll(".heart");
     hearts[life].classList.add("lost");
+    hearts[life-1].classList.add("animated-header");
 }
 
 document.addEventListener("DOMContentLoaded", function () {
